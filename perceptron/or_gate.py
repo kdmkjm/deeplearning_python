df0529fa01cd9a1e2bb25fa2849d73f8bbd99e09
kdmkjm <kdmkjm0@gmail.com>
2025-01-07 23:48:58 +0900

    20250107_perceptron

--- perceptron/or_gate.py
+++ perceptron/or_gate.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-def AND(x1, x2):
+def OR(x1, x2):
     x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.8
-    tmp = np.sum(w*x) + b
+    w = np.array([1.0, 1.0])
+    b = -0.9
+    tmp = np.sum(x*w) + b
     if tmp <= 0:
         return 0
-    elif tmp > 0:
+    else:
         return 1
     
 if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
-        y = AND(xs[0], xs[1])
-        print(str(xs) + " -> " + str(y))
+        y = OR(xs[0], xs[1])
+        print(str(xs) + ' -> ' + str(y))
