86a55b09ab11540ecd1976c3414affc9fa78f1e3
kdmkjm <kdmkjm0@gmail.com>
2025-01-11 04:29:29 +0900

    20240111_neural

---
+++ neural_network/neuralnet_mnist.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import pickle
+from dataset.mnist import load_mnist
+from common.functions import sigmoid, softmax
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=)
