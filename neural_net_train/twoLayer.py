8415494e9f6ca5b6ce1c1506a183c976c6481400
kdmkjm <kdmkjm@naver.com>
2025-01-26 19:17:04 +0900

    20250126

---
+++ neural_net_train/twoLayer.py
@@ -0,0 +1,78 @@
+import sys, os
+sys.path.append('C:/Users/1/OneDrive/Documents/GitHub/deeplearning_python')
+from common.functions import *
+from common.gradient import numerical_gradient
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    # 이미지 데이터 x를 활용한 예측을 수행.
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+    
+    # x : 입력 데이터, t : 정답 레이블
+    # 손실함수의 값을 구한다.
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+    
+    # 정확도를 구한다.
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        # axis 0 각 열에서 모든 행에 대해 동작(세로 진행방향)
+        # axis 1 각 행에서 모든 열에 대해 동작(가로 진행방향향)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np,sum(y == t) / float(x.shape[0])
+        return accuracy
+    
+    # 가중치 매개변수의 기울기 산출.
+    def numercial_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+    
+    # 가중치 매개변수의 기울기 산출.
+    # 상단의 numercial_gradient의 성능 개선판.
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
+
+        return grads
