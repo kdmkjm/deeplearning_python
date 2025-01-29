7e4755be7af4c48c2e3f4f5ef4d9275ccaa0bead
kdmkjm <kdmkjm0@gmail.com>
2025-01-29 23:25:35 +0900

    20250129_3

--- backpropagation/layer_func.py
+++ backpropagation/layer_func.py
@@ -1,3 +1,7 @@
+import sys, os
+# 데스크탑 패스
+sys.path.append('D:\Github\deeplearning_python')
+from common.functions import *
 import numpy as np
 
 class Relu:
@@ -29,5 +33,60 @@ class Sigmoid:
 
     def backward(self, dout):
         dx = dout * self.out * ( 1.0 - self.out )
+
+        return dx
+    
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        # 텐서 대응
+        self.original_x_shape = None
+
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        # 텐서 대응
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+
+        self.x = x
+        out = np.dot(x , self.W) + self.b
+
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.X.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        # 텐서 대응
+        dx = dx.reshape(*self.original_x_shape)
+        return dx
+    
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        # 정답 레이블이 원-핫 인코딩 형태를 경우
+        if self.t.size == self.y.size:
+            dx = ( self.y - self.t ) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
 
         return dx
